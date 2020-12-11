9ade4cb7170238566bc82bafa0a191b55f1c7027
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-11 21:24:33 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,4 +3,9 @@ class SightingsController < ApplicationController
     sighting = Sighting.find(params[:id])
     render json: SightingSerializer.new(sighting)
   end
+  
+  def index
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings)
+  end
 end
